af140cb996c192e904d7c2c459f2351f66ba2ab8
jopatk123 <chenshijian123123@gmail.com>
2025-08-10 09:04:27 +0800

    refactor(server): move videoPoint subcontrollers to kebab-case directory and update aggregator; remove old files.

--- server/src/controllers/video-point.controller.js
+++ server/src/controllers/video-point.controller.js
@@ -1,5 +1,5 @@
-const VideoPointQueryController = require('./videoPoint/videoPointQuery.controller')
-const VideoPointMutationController = require('./videoPoint/videoPointMutation.controller')
+const VideoPointQueryController = require('./video-point/video-point-query.controller')
+const VideoPointMutationController = require('./video-point/video-point-mutation.controller')
 
 class VideoPointController {
   static getVideoPoints = VideoPointQueryController.getVideoPoints
